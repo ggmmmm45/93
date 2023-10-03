b2f7aa2a0d0d8722f2aaf35ad4119a1121bacc69
ggmmmm45 <130798916+ggmmmm45@users.noreply.github.com>
2023-10-03 17:44:36 -0300

    bbb
    bztdeahbaedmbedosfjnohoerhjioj0erujtujeugioeiroititrtierujertujertijuerijuertiujertirteuijrtirtejuiortejuiretuirteurtegiouertiotugitu5jr4itui´tuiotujituertrupért

---
+++ kwitter.js
@@ -0,0 +1,19 @@
+// Defina a função.
+function addUser() {
+
+userName = document.getElementById("userName").Value;
+
+
+localStorage.setItem("userName" , userName);
+
+window.location = "kwitterRoom.html";
+
+}
+  // Obtenha o nome do usuário da input box e armazene dentro de uma variável
+
+  // Então, armazenamos o valor da variável userName no local storage com a palavra-chave userName.
+  
+  // Em seguida, redirecionamos para kwitterRoom.html.
+
+
+
